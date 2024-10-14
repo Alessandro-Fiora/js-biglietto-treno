fd5f7fa4d8d5dfdbabe9808c46f14b132a4118a6
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-14 14:58:40 +0200

    debugs removed

--- js/main.js
+++ js/main.js
@@ -5,17 +5,11 @@ const distance = parseInt(
   prompt("Inserisci il numero intero di KM che vuoi percorrere")
 );
 
-console.log(`distance: ${distance}, ${typeof distance}`);
-
 // ^ chiedo all'utente età del passeggero
 const age = parseInt(prompt("Inserisci l'età del passeggero"));
 
-console.log(`age: ${age}, ${typeof age}`);
-
 // ^ creo var prezzo al km
 let priceForKm = 0.21;
-
-console.log(`priceForKm: ${priceForKm}, ${typeof priceForKm}`);
 
 // ^ creo output
 let output;
@@ -27,35 +21,25 @@ let totPrice;
 
 // ^ controllo validità input distanza
 const isDistanceValid = !isNaN(distance) && distance > 0;
-console.log(
-  `prima dei controlli: isDistanceValid: ${isDistanceValid}, ${typeof isDistanceValid}`
-);
 
 // ^ controllo validità input età
 const isAgeValid = !isNaN(age) && age > 0;
-console.log(
-  `prima dei controlli: isAgeValid: ${isAgeValid}, ${typeof isAgeValid}`
-);
 
 // ^ SE input distanza E input età sono validi
 if (isDistanceValid && isAgeValid) {
   // ^ calcolo prezzo totale
   totPrice = distance * priceForKm;
-  console.log(`prima dei controlli: totPrice: ${totPrice}, ${typeof totPrice}`);
 
   // ^ aggiorno output con prezzo totale
   output = totPrice;
-  console.log(`prima dei controlli: output: ${output}, ${typeof output}`);
 
   // ^ SE l'età dell'utente è MINORE di 18 anni
   if (age < 18) {
     const discountPercentage = 20;
     // ^ applico sconto 20%
     totPrice = totPrice - totPrice * (discountPercentage / 100);
-    console.log(`MINORENNE: totPrice: ${totPrice}, ${typeof totPrice}`);
     // ^ aggiorno output
     output = totPrice;
-    console.log(`MINORENNE: output: ${output}, ${typeof output}`);
   }
 
   // ^ SE l'età dell'utente è MAGGIORE O UGUALE di 65 anni
@@ -63,10 +47,8 @@ if (isDistanceValid && isAgeValid) {
     const discountPercentage = 40;
     // ^ applico sconto 40%
     totPrice = totPrice - totPrice * (discountPercentage / 100);
-    console.log(`OVER 65: totPrice: ${totPrice}, ${typeof totPrice}`);
     // ^ aggiorno output
     output = totPrice;
-    console.log(`OVER 65: output: ${output}, ${typeof output}`);
   }
 
   // ! OUTPUT
