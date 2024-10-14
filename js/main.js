c5c321db3fe7a6e290de95ba499b32caddd98782
Alessandro-Fiora <fiora.ale97@gmail.com>
2024-10-14 14:41:40 +0200

    basics

--- js/main.js
+++ js/main.js
@@ -23,25 +23,38 @@ console.log(`priceForKm: ${priceForKm}, ${typeof priceForKm}`);
 
 let output;
 
-// ^ creo finalPrice
+// ^ creo totPrice
 
-let finalPrice;
+let totPrice;
 
 // ! ELABORAZIONE
 
-finalPrice = distance * priceForKm;
-console.log(`finalPrice: ${finalPrice}, ${typeof finalPrice}`);
+totPrice = distance * priceForKm;
+console.log(`prima dei controlli: totPrice: ${totPrice}, ${typeof totPrice}`);
 
 // ^ SE l'età dell'utente è MINORE di 18 anni
 if (age < 18) {
+  const discountPercentage = 20;
+  // ^ applico sconto 20%
+  totPrice = totPrice - totPrice * (discountPercentage / 100);
+  console.log(`MINORENNE: totPrice: ${totPrice}, ${typeof totPrice}`);
+  // ^ aggiorno output
+  output = totPrice;
+  console.log(`MINORENNE: output: ${output}, ${typeof output}`);
 }
-// ^ applico sconto 20%
-// ^ aggiorno output
 
 // ^ SE l'età dell'utente è MAGGIORE di 65 anni
-// ^ applico sconto 40%
-// ^ aggiorno output
+else if (age > 65) {
+  const discountPercentage = 40;
+  // ^ applico sconto 40%
+  totPrice = totPrice - totPrice * (discountPercentage / 100);
+  console.log(`OVER 65: totPrice: ${totPrice}, ${typeof totPrice}`);
+  // ^ aggiorno output
+  output = totPrice;
+  console.log(`OVER 65: output: ${output}, ${typeof output}`);
+}
 
 // ! OUTPUT
 
 // ^ STAMPO output in forma umana (max 2 decimali)
+alert(`${output.toFixed(2)}€`);
